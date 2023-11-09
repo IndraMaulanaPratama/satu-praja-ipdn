7b471feaff0d39e2e0d42110774a1276a4da04b2
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-11-10 05:03:29 +0700

    UPDATE || Change value seeder on role and user seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,9 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\Role;
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class UserSeeder extends Seeder
@@ -14,6 +12,6 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory()->count(30)->create();
+        User::factory()->count(3)->create();
     }
 }
