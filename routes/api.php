9e03e49bf25546718b29c976866c5da7f3a7c7bc
IndraMaulanaPratama <maulana.indra808@gmail.com>
2024-07-18 15:58:38 +0700

    ADD NEW || Route for count data praja on route api

--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/praja/', [PrajaController::class, 'getData']);
+Route::get('/praja/count/', [PrajaController::class, 'count']);
