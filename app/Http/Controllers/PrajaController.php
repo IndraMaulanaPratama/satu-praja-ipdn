7a00607ec7702eac9859b134551ff50046cc1e63
IndraMaulanaPratama <maulana.indra808@gmail.com>
2024-07-22 14:25:48 +0700

    fixing bug data praja

--- app/Http/Controllers/PrajaController.php
+++ app/Http/Controllers/PrajaController.php
@@ -10,6 +10,39 @@ class PrajaController extends Controller
 {
     public function getData(Request $request)
     {
+        try {
+            $data = [
+                'npp' => $request->npp ?? null,
+                'nama' => $request->nama ?? null,
+                'jenis_kelamin' => $request->jenis_kelamin ?? null,
+                'agama' => $request->agama ?? null,
+                'tingkat' => $request->tingkat ?? null,
+                'angkatan' => $request->angkatan ?? null,
+                'kampus' => $request->kampus ?? null,
+                'wisma' => $request->wisma ?? null,
+                'program_pendidikan' => $request->program_pendidikan ?? null,
+                'fakultas' => $request->fakultas ?? null,
+                'program_studi' => $request->program_studi ?? null,
+                'kelas' => $request->kelas ?? null,
+                'nomor_ponsel' => $request->nomor_ponsel ?? null,
+                'item' => $request->item ?? null,
+            ];
+
+            $result = Praja::getPraja($data);
+
+            if (count($result) != false) {
+                return response()->json($result);
+            } else {
+                return response()->json(['Pesan' => 'Data Praja tidak ditemukan'], 404);
+            }
+        } catch (\Throwable $th) {
+            throw new Exception($th->getMessage(), 500);
+        }
+    }
+
+
+    public function searchData(Request $request)
+    {
         // ngadamel variabel dumasar kana data anu dikintun ku semah
         [
             "npp" => $npp,
@@ -94,11 +127,5 @@ class PrajaController extends Controller
         } catch (\Throwable $th) {
             return response()->json(['message' => $th->getMessage()], 500);
         }
-    }
-
-
-    public function count(Request $request)
-    {
-        return $request;
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -19,5 +19,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/praja/', [PrajaController::class, 'getData']);
-Route::post('/praja/count/', [PrajaController::class, 'count']);
+Route::get('/praja/', [PrajaController::class, 'getData']);
+Route::post('/praja/', [PrajaController::class, 'searchData']);
