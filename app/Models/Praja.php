7678632f955fd33115856a908a84de86bfcb4305
IndraMaulanaPratama <maulana.indra808@gmail.com>
2024-07-22 14:02:25 +0700

    UPDATE || Function model praja

--- app/Models/Praja.php
+++ app/Models/Praja.php
@@ -39,7 +39,30 @@ class Praja extends Model
         'deleted_at'
     ];
 
-    public function scopeGetPraja(...$search)
+    public function scopeGeneralData($query)
+    {
+        return $query->select(
+            'PRAJA_NPP as npp',
+            'PRAJA_EMAIL as email',
+            'PRAJA_NAMA as nama',
+            'PRAJA_TEMPAT_LAHIR as tempat_lahir',
+            'PRAJA_TANGGAL_LAHIR as tanggal_lahir',
+            'PRAJA_JENIS_KELAMIN as jenis_kelamin',
+            'PRAJA_PROVINSI as provinsi',
+            'PRAJA_KOTA as kota',
+            'PRAJA_AGAMA as agama',
+            'PRAJA_TINGKAT as tingkat',
+            'PRAJA_ANGKATAN as angkatan',
+            'PRAJA_KAMPUS as kampus',
+            'PRAJA_WISMA as wisma',
+            'PRAJA_PROGRAM_PENDIDIKAN as program_pendidikan',
+            'PRAJA_FAKULTAS as fakultas',
+            'PRAJA_PROGRAM_STUDI as program_studi',
+            'PRAJA_KELAS as kelas',
+        );
+    }
+
+    public function scopeGetPraja($query, ...$search)
     {
 
         return Praja::query()
@@ -147,13 +170,155 @@ class Praja extends Model
             );
     }
 
-    public function scopeCountPraja($data)
+    public function scopeWhenNpp($query, $data)
     {
-        return Praja::when(
+        return $query->when(
             $data,
             function ($query, $data) {
-                return $query->where($data['field'], 'LIKE', '%' . $data['record'] . '%');
+                return $query->where('PRAJA_NPP', $data);
             }
-        )->count();
+        );
+
     }
+
+    public function scopeWhenNama($query, $data)
+    {
+        return $query->when(
+            $data,
+            function ($query, $data) {
+                return $query->where('PRAJA_NAMA', 'LIKE', "%$data%");
+            }
+        );
+    }
+
+    public function scopeWhenTempatLahir($query, $data)
+    {
+        return $query->when(
+            $data,
+            function ($query, $data) {
+                return $query->where('PRAJA_TEMPAT_LAHIR', 'LIKE', "$data%");
+            }
+        );
+    }
+
+    public function scopeWhenJenisKelamin($query, $data)
+    {
+        return $query->when(
+            $data,
+            function ($query, $data) {
+                return $query->where('PRAJA_JENIS_KELAMIN', $data);
+            }
+        );
+    }
+
+    public function scopeWhenProvinsi($query, $data)
+    {
+        return $query->when(
+            $data,
+            function ($query, $data) {
+                return $query->where('PRAJA_PROVINSI', 'LIKE', "$data%");
+            }
+        );
+    }
+
+    public function scopeWhenKota($query, $data)
+    {
+        return $query->when(
+            $data,
+            function ($query, $data) {
+                return $query->where('PRAJA_KOTA', 'LIKE', "$data%");
+            }
+        );
+    }
+
+    public function scopeWhenAgama($query, $data)
+    {
+        return $query->when(
+            $data,
+            function ($query, $data) {
+                return $query->where('PRAJA_AGAMA', 'LIKE', "$data%");
+            }
+        );
+    }
+
+    public function scopeWhenTingkat($query, $data)
+    {
+        return $query->when(
+            $data,
+            function ($query, $data) {
+                return $query->where('PRAJA_TINGKAT', 'LIKE', "$data%");
+            }
+        );
+    }
+
+    public function scopeWhenAngkatan($query, $data)
+    {
+        return $query->when(
+            $data,
+            function ($query, $data) {
+                return $query->where('PRAJA_ANGKATAN', 'LIKE', "$data%");
+            }
+        );
+    }
+
+    public function scopeWhenKampus($query, $data)
+    {
+        return $query->when(
+            $data,
+            function ($query, $data) {
+                return $query->where('PRAJA_KAMPUS', 'LIKE', "$data%");
+            }
+        );
+    }
+
+    public function scopeWhenWisma($query, $data)
+    {
+        return $query->when(
+            $data,
+            function ($query, $data) {
+                return $query->where('PRAJA_WISMA', 'LIKE', "$data%");
+            }
+        );
+    }
+
+    public function scopeWhenProgramPendidikan($query, $data)
+    {
+        return $query->when(
+            $data,
+            function ($query, $data) {
+                return $query->where('PRAJA_PROGRAM_PENDIDIKAN', $data);
+            }
+        );
+    }
+
+    public function scopeWhenFakultas($query, $data)
+    {
+        return $query->when(
+            $data,
+            function ($query, $data) {
+                return $query->where('PRAJA_FAKULTAS', $data);
+            }
+        );
+    }
+
+    public function scopeWhenProgramStudi($query, $data)
+    {
+        return $query->when(
+            $data,
+            function ($query, $data) {
+                return $query->where('PRAJA_PROGRAM_STUDI', $data);
+            }
+        );
+    }
+
+    public function scopeWhenKelas($query, $data)
+    {
+        return $query->when(
+            $data,
+            function ($query, $data) {
+                return $query->where('PRAJA_KELAS', $data);
+            }
+        );
+    }
+
 }
