5028de88b6f16557780fff4f7f57917d2b0eb73c
IndraMaulanaPratama <maulana.indra808@gmail.com>
2024-07-22 14:02:04 +0700

    UPDATE || Function get data praja on controller praja

--- app/Http/Controllers/PrajaController.php
+++ app/Http/Controllers/PrajaController.php
@@ -10,123 +10,95 @@ class PrajaController extends Controller
 {
     public function getData(Request $request)
     {
+        // ngadamel variabel dumasar kana data anu dikintun ku semah
+        [
+            "npp" => $npp,
+            "nama" => $nama,
+            "tempat_lahir" => $tempat_lahir,
+            "jenis_kelamin" => $jenis_kelamin,
+            "provinsi" => $provinsi,
+            "kota" => $kota,
+            "agama" => $agama,
+            "tingkat" => $tingkat,
+            "angkatan" => $angkatan,
+            "kampus" => $kampus,
+            "wisma" => $wisma,
+            "program_pendidikan" => $program_pendidikan,
+            "fakultas" => $fakultas,
+            "program_studi" => $program_studi,
+            "kelas" => $kelas,
+            "item" => $item,
+        ] = $request;
+
         try {
-            $data = [
-                'npp' => $request->npp ?? null,
-                'nama' => $request->nama ?? null,
-                'jenis_kelamin' => $request->jenis_kelamin ?? null,
-                'agama' => $request->agama ?? null,
-                'tingkat' => $request->tingkat ?? null,
-                'angkatan' => $request->angkatan ?? null,
-                'kampus' => $request->kampus ?? null,
-                'wisma' => $request->wisma ?? null,
-                'program_pendidikan' => $request->program_pendidikan ?? null,
-                'fakultas' => $request->fakultas ?? null,
-                'program_studi' => $request->program_studi ?? null,
-                'kelas' => $request->kelas ?? null,
-                'nomor_ponsel' => $request->nomor_ponsel ?? null,
-                'item' => $request->item ?? null,
-            ];
+            // Logika kanggo maca database
+            $data = Praja::generalData()
 
-            $result = Praja::getPraja($data);
+                // Milari data dumasar kana npp
+                ->whenNpp($npp)
 
-            if (count($result) != false) {
-                return response()->json($result);
-            } else {
-                return response()->json(['Pesan' => 'Data Praja tidak ditemukan'], 404);
-            }
+                // Milari data dumasar kana nami
+                ->whenNama($nama)
+
+                // Milari data dumasar kana tempat lahir
+                ->whenTempatLahir($tempat_lahir)
+
+                // Milari data dumasar kana jenis kelamin
+                ->whenJenisKelamin($jenis_kelamin)
+
+                // Milari data dumasar kana provinsi
+                ->whenProvinsi($provinsi)
+
+                // Milari data dumasar kana kota
+                ->whenKota($kota)
+
+                // Milari data dumasar kana ageman
+                ->whenAgama($agama)
+
+                // Milari data dumasar kana tingkat
+                ->whenTingkat($tingkat)
+
+                // Milari data dumasar kana angkatan
+                ->whenAngkatan($angkatan)
+
+                // Milari data dumasar kana kampus
+                ->whenKampus($kampus)
+
+                // Milari data dumasar kana wisma
+                ->whenWisma($wisma)
+
+                // Milari data dumasar kana program pendidikan
+                ->whenProgramPendidikan($program_pendidikan)
+
+                // Milari data dumasar kana fakultas
+                ->whenFakultas($fakultas)
+
+                // Milari data dumasar kana program studi
+                ->whenProgramStudi($program_studi)
+
+                // Milari data dumasar kana kelas
+                ->whenKelas($kelas)
+
+                ->paginate($item);
+            // Tungtung tina logika kanggo maca database
+
+            // Ngadamel variable pesan sareng kode status dumasar kana hasil data
+            $message = (count($data) == null) ? 'Data praja tidak ditemukan' : 'Data praja berhasil ditemukan';
+            $status = (count($data) == null) ? 400 : 200;
+            $response = (count($data) == null) ? null : $data;
+
+            // Mulangkeun hasil ka semah
+            return response()->json(['message' => $message, 'data' => $response], $status);
+
+
         } catch (\Throwable $th) {
-            throw new Exception($th->getMessage(), 500);
+            return response()->json(['message' => $th->getMessage()], 500);
         }
     }
 
 
     public function count(Request $request)
     {
-        try {
-            $message = 'Berhasil melakukan perhitungan data praja';
-
-            // All Data
-            if ('all' == $request['field']):
-                $response = Praja::count();
-
-                // Jenis Kelamin
-            elseif ('jenis_kelamin' == $request['field']):
-                $data = ['field' => 'PRAJA_JENIS_KELAMIN', 'record' => $request['record']];
-                $response = Praja::countPraja($data);
-
-                // Provinsi
-            elseif ('provinsi' == $request['field']):
-                $data = ['field' => 'PRAJA_PROVINSI', 'record' => $request['record']];
-                $response = Praja::countPraja($data);
-
-                // Kota
-            elseif ('kota' == $request['field']):
-                $data = ['field' => 'PRAJA_KOTA', 'record' => $request['record']];
-                $response = Praja::countPraja($data);
-
-                // Agama
-            elseif ('agama' == $request['field']):
-                $data = ['field' => 'PRAJA_AGAMA', 'record' => $request['record']];
-                $response = Praja::countPraja($data);
-
-                // Tingkat
-            elseif ('tingkat' == $request['field']):
-                $data = ['field' => 'PRAJA_TINGKAT', 'record' => $request['record']];
-                $response = Praja::countPraja($data);
-
-                // Angkatan
-            elseif ('angkatan' == $request['field']):
-                $data = ['field' => 'PRAJA_ANGKATAN', 'record' => $request['record']];
-                $response = Praja::countPraja($data);
-
-                // Kampus
-            elseif ('kampus' == $request['field']):
-                $data = ['field' => 'PRAJA_KAMPUS', 'record' => $request['record']];
-                $response = Praja::countPraja($data);
-
-                // Wisma
-            elseif ('wisma' == $request['field']):
-                $data = ['field' => 'PRAJA_WISMA', 'record' => $request['record']];
-                $response = Praja::countPraja($data);
-
-                // Program Pendidikan
-            elseif ('program_pendidikan' == $request['field']):
-                $data = ['field' => 'PRAJA_PROGRAM_PENDIDIKAN', 'record' => $request['record']];
-                $response = Praja::countPraja($data);
-
-                // Fakultas
-            elseif ('fakultas' == $request['field']):
-                $data = ['field' => 'PRAJA_FAKULTAS', 'record' => $request['record']];
-                $response = Praja::countPraja($data);
-
-                // Program Studi
-            elseif ('program_studi' == $request['field']):
-                $data = ['field' => 'PRAJA_PROGRAM_STUDI', 'record' => $request['record']];
-                $response = Praja::countPraja($data);
-
-                // Kelas
-            elseif ('kelas' == $request['field']):
-                $data = ['field' => 'PRAJA_KELAS', 'record' => $request['record']];
-                $response = Praja::countPraja($data);
-                // Field teu sesuai
-            else:
-                $response = null;
-            endif;
-
-            // Ngarobih pesan nalika field teu sesuai sareng katangtosan
-            if (is_null($response)):
-                $message = 'Data praja tidak ditemukan';
-            endif;
-
-            // Ngirimkeun data hasil tinu proses ka semah
-            return response()->json([
-                'message' => $message,
-                'data' => ['total' => $response],
-            ]);
-
-        } catch (\Throwable $th) {
-            return response()->json(['message' => $th->getMessage()], 500);
-        }
+        return $request;
     }
 }
